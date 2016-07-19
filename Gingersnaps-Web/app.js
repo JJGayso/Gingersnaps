db098563ee532ee94835c1ef40c70a9f5e7ca1dd
JJGayso <gaysojj@rose-hulman.edu>
2016-07-18 22:23:16 -0400

    Getting Familiar with Mustache, sample layout and page created

--- Gingersnaps-Web/app.js
+++ Gingersnaps-Web/app.js
@@ -11,8 +11,9 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+// Use `.hbs` for extensions and find partials in `views/partials`.
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,38 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 
 module.exports = app;
