4fabc66c0ac664d1ee675f228ee17c5cd953433d
JJGayso <gaysojj@rose-hulman.edu>
2016-07-20 23:01:18 -0400

    Added Food menu feature block, also added jquery for reaching home and food

--- Gingersnaps-Web/public/javascripts/navbarTransitions.js
+++ Gingersnaps-Web/public/javascripts/navbarTransitions.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
+
+  $("#homeButton").click(function(){
+    $('html, body').animate({
+      scrollTop: $("#myCarousel").offset().top - 60
+    }, 1000);
+  });
+
   $("#drinkMenuButton").click(function(){
     $('html, body').animate({
       scrollTop: $("#drinkMenu").offset().top - 60
+    }, 1000);
+  });
+
+  $("#foodMenuButton").click(function(){
+    $('html, body').animate({
+      scrollTop: $("#foodMenu").offset().top - 60
     }, 1000);
   });
 });
